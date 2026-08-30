98904076da73d7dce7279ecb0e3440adc1f1c81c
Anarok404 <anmolsxn2005@gmail.com>
2026-08-30 15:17:35 +0530

    added logging service

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,8 @@ import rateLimit from "express-rate-limit";
 import passport from "./config/passport.js";
 import { authMiddleware } from "./middleware/auth.middleware.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
+import { globalLogger } from "./middleware/logger.middleware.js";
+
 
 import authRoutes from "./modules/auth/auth.routes.js";
 import communityRoutes from "./modules/community/community.routes.js";
@@ -54,6 +56,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+app.use(globalLogger)
 
 app.get("/health", (req, res) => {
   res.status(200).json({
